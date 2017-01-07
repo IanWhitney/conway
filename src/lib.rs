f1c4bb8e80753bed1903dc220b0a0121ea3d71dc
Ian Whitney <whit0694@umn.edu>
2017-01-07 14:52:31 -0600

    Extract world
    Trying a different approach here. Last time
    (d7230bcf70ffb92a46f5aea351b6fe758428f754) I tried to extract just Rows
    and found it painful.
    
    This time I'm trying to extract a World, mostly with an eye on
    simplifying `living_neighbors_of`
    
    In this approach, the `Conway` struct is home to the `tick` logic and
    how to output the world to a string.
    
    The `World` is a collection of `Location`s, each of which holds a
    `Cell`. It also holds a private state, which our good-old nested Vector.
    
    Location gets some useful logic here, being able to return the neighbors
    of itself. Current approach is a bit brute-forcey, but it's not like
    it's ever going to need to change
    
    With this, a world can return the valid neighbors of any location, which
    e can then use to determine the number of living cells neighboring any
    other cell.
    
    This works better than just trying to extract Rows. I think the Row
    concept wasn't a full-fledged thing in the system, which is what made it
    so hard to extract.

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,41 @@
 pub struct Conway {
-    rows: Vec<Vec<Cell>>,
+    world: World,
 }
 
 impl Conway {
     pub fn new(row_count: usize) -> Self {
-        let mut rows: Vec<Vec<Cell>> = Vec::new();
+        let world = World::new(&20, &row_count);
 
-        for _ in 0..row_count {
-            let mut row: Vec<Cell> = Vec::new();
-            for _ in 0..20 {
-                row.push(Cell::dead());
-            }
-            rows.push(row)
-        }
-
-        Conway { rows: rows }
+        Conway { world: world }
     }
 
     pub fn state(&self) -> String {
         let mut r = String::from("");
 
-        for row in self.rows.iter() {
-            for c in row.iter() {
-                r.push(c.state());
+        let mut row = 0;
+
+        for location in self.world.locations.iter() {
+            if location.y != row {
+                row += 1;
+                r.push('\n');
             }
-            r.push('\n');
+
+            if let Some(cell) = self.world.cell_at(&location) {
+                r.push(cell.state());
+            }
         }
-        r.pop();
         r
     }
 
     pub fn add_living(&mut self, l: &Location) {
-        self.rows[l.y][l.x] = Cell::alive()
+        self.world.add(Cell::alive(), l);
     }
 
     pub fn tick(&self) -> Conway {
-        let mut new_conway = Conway::new(self.rows.len());
+        let mut new_conway = Conway::new(self.world.height);
 
-        for (y_index, row) in self.rows.iter().enumerate() {
-            for (x_index, cell) in row.iter().enumerate() {
-                let location = Location::new(x_index, y_index);
+        for location in self.world.locations.iter() {
+            if let Some(cell) = self.world.cell_at(&location) {
                 let living_cell = cell.living;
                 let living_neighbors = self.living_neighbor_count(&location);
 
@@ -52,40 +48,21 @@ impl Conway {
                 }
             }
         }
+
         new_conway
     }
 
     fn living_neighbor_count(&self, location: &Location) -> usize {
-        let y = location.y;
-        let x = location.x;
-
-        let low_y = if y < 1 { 0 } else { y - 1 };
-        let high_y = if y == (self.rows.len() - 1) {
-            self.rows.len()
-        } else {
-            y + 2
-        };
-
-        let mut count = 0;
-
-        for y_index in low_y..high_y {
-            if x >= 1 && self.rows[y_index][x - 1].living {
-                count += 1;
-            }
-
-            if x <= 18 && self.rows[y_index][x + 1].living {
-                count += 1;
-            }
-
-            if y_index != y && self.rows[y_index][x].living {
-                count += 1;
-            }
-
-        }
-        count
+        self.world
+            .neighbors_of(location)
+            .into_iter()
+            .filter(|&c| c.living)
+            .collect::<Vec<&Cell>>()
+            .len()
     }
 }
 
+#[derive(Debug)]
 pub struct Location {
     x: usize,
     y: usize,
@@ -95,8 +72,32 @@ impl Location {
     pub fn new(x: usize, y: usize) -> Self {
         Location { x: x, y: y }
     }
+
+    fn neighbors(&self) -> Vec<Location> {
+        let mut n = Vec::new();
+
+        if self.x > 0 && self.y > 0 {
+            n.push(Location::new(self.x - 1, self.y - 1));
+        }
+
+        if self.x > 0 {
+            n.push(Location::new(self.x - 1, self.y));
+            n.push(Location::new(self.x - 1, self.y + 1));
+        }
+
+        if self.y > 0 {
+            n.push(Location::new(self.x, self.y - 1));
+            n.push(Location::new(self.x + 1, self.y - 1));
+        }
+
+        n.push(Location::new(self.x + 1, self.y));
+        n.push(Location::new(self.x, self.y + 1));
+        n.push(Location::new(self.x + 1, self.y + 1));
+        n
+    }
 }
 
+#[derive(Debug)]
 struct Cell {
     living: bool,
 }
@@ -112,5 +113,64 @@ impl Cell {
 
     pub fn state(&self) -> char {
         if self.living { 'X' } else { 'O' }
+    }
+}
+
+struct World {
+    height: usize,
+    state: Vec<Vec<Cell>>,
+    locations: Vec<Location>,
+}
+
+impl World {
+    fn new(width: &usize, height: &usize) -> Self {
+        let mut rows: Vec<Vec<Cell>> = Vec::new();
+        let mut locations: Vec<Location> = Vec::new();
+
+        for y_index in 0..height + 0 {
+            let mut row: Vec<Cell> = Vec::new();
+            for x_index in 0..width + 0 {
+                row.push(Cell::dead());
+                locations.push(Location::new(x_index, y_index));
+            }
+            rows.push(row);
+        }
+
+        World {
+            height: height.clone(),
+            state: rows,
+            locations: locations,
+        }
+    }
+
+    fn add(&mut self, cell: Cell, location: &Location) {
+        self.state[location.y][location.x] = cell;
+    }
+
+    fn cell_at(&self, location: &Location) -> Option<&Cell> {
+        if let Some(_) = self.state.get(location.y) {
+            if let Some(cell) = self.state[location.y].get(location.x) {
+                Some(&cell)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn neighbors_of(&self, location: &Location) -> Vec<&Cell> {
+        let neighbors = location.neighbors();
+
+        let mut cells = Vec::new();
+
+        for neighbor in neighbors {
+            if let Some(_) = self.state.get(neighbor.y) {
+                if let Some(cell) = self.state[neighbor.y].get(neighbor.x) {
+                    cells.push(cell);
+                }
+            }
+        }
+        cells
     }
 }
